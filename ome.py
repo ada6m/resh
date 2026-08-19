dd6c1816d27b9532e254138f3a24a856bb85b9fb
adam <36342604+bomboclaat6@users.noreply.github.com>
2026-08-19 00:21:34 -0400

    Update ome.py

--- ome.py
+++ ome.py
@@ -2,12 +2,17 @@ import tifffile
 import dask.array as da
 from lxml import etree
 
+def load_file(path, channel):
+    store = tifffile.imread(path, aszarr=True)
+    img = da.from_zarr(store) #use zarr to lazy load .ome
+    return img[channel]
+
 def read_metadata(path):
     metadata = []
     with tifffile.TiffFile(path) as tif: #open .ome.tif file
-        xml = tif.ome_metadata #only read xml metadata
-    root = etree.fromstring(xml.encode()) #set root
-    ns = {"ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06"} #set namespace
+        xml = tif.ome_metadata 
+    root = etree.fromstring(xml.encode()) 
+    ns = {"ome": "http://www.openmicroscopy.org/Schemas/OME/2016-06"} 
     images = root.findall("ome:Image", ns) #find all image instances
     for image in images:
         pixels = image.find("ome:Pixels", ns) #find a pixel instance
@@ -21,8 +26,3 @@ def read_metadata(path):
             ]
         })
     if metadata is not None: return metadata
-
-def load_file(path, channel):
-    store = tifffile.imread(path, aszarr=True)
-    img = da.from_zarr(store)
-    return img[channel]
